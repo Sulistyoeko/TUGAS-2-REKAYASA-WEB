65f3ae998290878f91aa01745be45854676ec20d
Sulistyoeko <164310505+Sulistyoeko@users.noreply.github.com>
2025-10-06 08:44:44 +0700

    Add files via upload

---
+++ PRAK2/tugas.php
@@ -0,0 +1,36 @@
+<?php 
+function curl($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
+// Ambil data JSON dari file PHP lain
+$send = curl("http://localhost/rekayasaweb/PRAK2/getWisata.php");
+
+// Decode JSON ke array asosiatif
+$data = json_decode($send, TRUE);
+
+// Tampilkan ke HTML Tabel
+echo "<table border='1' cellspacing='0' cellpadding='8'>";
+echo "<tr style='background-color: #f2f2f2; font-weight:bold;'>
+        <td>KOTA</td>
+        <td>LANDMARK</td>
+        <td>TARIF</td>
+      </tr>";
+
+foreach ($data as $row) {
+    echo "<tr>
+            <td>".$row['kota']."</td>
+            <td>".$row['landmark']."</td>
+            <td>".$row['tarif']."</td>
+          </tr>";
+}
+
+
+echo "</table>";
+?>
+
